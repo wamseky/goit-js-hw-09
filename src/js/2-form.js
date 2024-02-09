b04eeb609eddc867a3920609d33fc4625c09bd2c
Diana Kharchenko <vitadiana2@gmail.com>
2024-02-09 20:42:58 +0200

    .

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,18 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const form = document.querySelector(".feedback-form");
+form.addEventListener("submit", handleSubmit);
 
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = { email, password };
+  console.log(formData);
+  form.reset();
+}
